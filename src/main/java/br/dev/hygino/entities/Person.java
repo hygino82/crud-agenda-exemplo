c274d6c43265b88d498b0ad99bbc1015461da9bb
Adroaldo <hygino@msn.com>
2024-04-17 15:27:57 -0300

    H2 Database seed, Find All Persons

--- src/main/java/br/dev/hygino/entities/Person.java
+++ src/main/java/br/dev/hygino/entities/Person.java
@@ -14,7 +14,8 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
     private String address;
     private String zipCode;
@@ -31,12 +32,20 @@ public class Person {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
